545f4e98b4387fb6febe8b7d13bc23dd58c0126f
oguzhanpn <68432409+oguzhanpn@users.noreply.github.com>
2025-02-10 18:32:32 +0000

    update imports & beautify

--- dca_v0.py
+++ dca_v0.py
@@ -1,8 +1,8 @@
-from aizona_strategies.base_strategy import BaseStrategy
+from base_strategy import BaseStrategy
 
 
 class strategy(BaseStrategy):
-    strategy_name="dca_v0"
+    strategy_name = "dca_v0"
 
     def __init__(self, pair_list, params, data_dir_name=None):
         super().__init__(pair_list, params, self.strategy_name, data_dir_name)
@@ -14,7 +14,8 @@ class strategy(BaseStrategy):
                 if self.params['side'] == "LONG":
                     # put start order
                     price = self.get_bid_price(step)
-                    _dict = {"pair": pair, "side": "buy", "price": price, "amount": self.params['order_amount']/price, "step": step}
+                    _dict = {"pair": pair, "side": "buy", "price": price, "amount": self.params['order_amount'] / price,
+                             "step": step}
                     self.create_limit_order(_dict)
                 else:
                     # wait do nothing - to be implemented..
@@ -22,13 +23,15 @@ class strategy(BaseStrategy):
             else:
                 if self.params['side'] == "LONG":
                     # add dca order
-                    price = self.get_last_trade_buy_price() * (1-self.params['price_deviation_to_safety_order']/100)
-                    _dict = {"pair": pair, "side": "buy", "price": price, "amount": self.params['order_amount']/price, "step": step}
+                    price = self.get_last_trade_buy_price() * (1 - self.params['price_deviation_to_safety_order'] / 100)
+                    _dict = {"pair": pair, "side": "buy", "price": price, "amount": self.params['order_amount'] / price,
+                             "step": step}
                     self.create_limit_order(_dict)
                 else:
-                    #close pos order
-                    price = self.get_open_position_price(pair) * (1-self.params['take_profit']/100)
-                    _dict = {"pair": pair, "side": "buy", "price": price, "amount": self.get_position(pair), "step": step}
+                    # close pos order
+                    price = self.get_open_position_price(pair) * (1 - self.params['take_profit'] / 100)
+                    _dict = {"pair": pair, "side": "buy", "price": price, "amount": self.get_position(pair),
+                             "step": step}
                     self.create_limit_order(_dict)
 
     def control_sell_limit_order(self, pair):
@@ -38,7 +41,8 @@ class strategy(BaseStrategy):
                 if self.params['side'] == "SHORT":
                     # put start order
                     price = self.get_ask_price(step)
-                    _dict = {"pair": pair, "side": "sell", "price": price, "amount": self.params['order_amount']/price, "step": step}
+                    _dict = {"pair": pair, "side": "sell", "price": price,
+                             "amount": self.params['order_amount'] / price, "step": step}
                     self.create_limit_order(_dict)
                 else:
                     # wait do nothing
@@ -46,19 +50,22 @@ class strategy(BaseStrategy):
             else:
                 if self.params['side'] == "SHORT":
                     # add dca order
-                    price = self.get_last_trade_sell_price() * (1+self.params['price_deviation_to_safety_order']/100)
-                    _dict = {"pair": pair, "side": "sell", "price": price, "amount": self.params['order_amount']/price, "step": step}
+                    price = self.get_last_trade_sell_price() * (
+                                1 + self.params['price_deviation_to_safety_order'] / 100)
+                    _dict = {"pair": pair, "side": "sell", "price": price,
+                             "amount": self.params['order_amount'] / price, "step": step}
                     self.create_limit_order(_dict)
                 else:
-                    #close pos order
-                    price = self.get_open_position_price(pair) * (1+self.params['take_profit']/100)
-                    _dict = {"pair": pair, "side": "sell", "price": price, "amount": self.get_position(pair), "step": step}
+                    # close pos order
+                    price = self.get_open_position_price(pair) * (1 + self.params['take_profit'] / 100)
+                    _dict = {"pair": pair, "side": "sell", "price": price, "amount": self.get_position(pair),
+                             "step": step}
                     self.create_limit_order(_dict)
 
     def cancel_control_buy_limit_order(self, pair):
         step = self.get_current_step()
         buy_orders = self.get_buy_orders(pair)
-        
+
         if len(buy_orders) != 0:
             if self.get_position(pair) == 0:
                 if step - buy_orders[0].step > 60:
@@ -67,7 +74,7 @@ class strategy(BaseStrategy):
     def cancel_control_sell_limit_order(self, pair):
         step = self.get_current_step()
         sell_orders = self.get_sell_orders(pair)
-        
+
         if len(sell_orders) != 0:
             if self.get_position(pair) == 0:
                 if step - sell_orders[0].step > 60:
@@ -80,21 +87,16 @@ class strategy(BaseStrategy):
         self.cancel_control_sell_limit_order(pair)
 
 
-if __name__=="__main__":
-
-    params={
+if __name__ == "__main__":
+    params = {
         "side": "LONG",
         "order_amount": 1000,
         "take_profit": 0.2,
         "price_deviation_to_safety_order": 0.1}
 
-    self=strategy(
-        pair_list = ["BTC"],
+    self = strategy(
+        pair_list=["BTC"],
         params=params,
         data_dir_name='tardis_data_test/'
     )
     self.run_strategyy()
-
-
-
-
