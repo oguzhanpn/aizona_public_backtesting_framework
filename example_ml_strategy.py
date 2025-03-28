e03d00bd7481803321d6d0d8454d00a2436b1a74
oguzhanpn <68432409+oguzhanpn@users.noreply.github.com>
2025-03-28 16:37:32 +0300

    accuracy metrics will be sent & take profit integration

--- example_ml_strategy.py
+++ example_ml_strategy.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 from base_strategy import BaseStrategy
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from base_ml_model import BaseModel
 import pandas as pd
 
@@ -29,6 +29,9 @@ class Model(BaseModel):
         # Calculate labels (price movement direction)
         # 1 for price increase, 0 for price decrease
         labels = (train_df['price'].shift(-1) > train_df['price']).astype(int)
+
+        # Random downsample the data
+        features_df, labels = self.random_downsample(features_df, labels)
         
         # Remove the last row since it won't have a label
         features_df = features_df.iloc[:-1]
@@ -39,7 +42,7 @@ class Model(BaseModel):
         X = features_df[valid_rows]
         y = labels[valid_rows]
         # Train the model
-        self.model = RandomForestClassifier(n_estimators=100, random_state=42)
+        self.model = XGBClassifier(n_estimators=100, random_state=56)
         self.model.fit(X, y)
         
         # Store feature names
@@ -167,11 +170,22 @@ class Model(BaseModel):
         f1_score = 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
         
         return {    
-            'accuracy': accuracy,
-            'precision': precision,
-            'recall': recall,
-            'f1_score': f1_score
+            'accuracy': np.round(accuracy, 2),
+            'precision': np.round(precision, 2),
+            'recall': np.round(recall, 2),
+            'f1_score': np.round(f1_score, 2)
         }
+
+    def random_downsample(self, features_df, labels):
+        """Randomly downsample the data"""
+        # Randomly select balanced data
+        pos_labels = labels[labels == 1]    
+        neg_labels = labels[labels == 0]
+        sample_size = min(len(pos_labels), len(neg_labels))
+        pos_indices = np.random.choice(range(len(pos_labels)), size=sample_size, replace=False)
+        neg_indices = np.random.choice(range(len(neg_labels)), size=sample_size, replace=False)
+        selected_indices = np.concatenate([pos_indices, neg_indices])
+        return features_df.iloc[selected_indices], labels.iloc[selected_indices]
 
 class Strategy(BaseStrategy):
     """Example ML model strategy Implementation"""
@@ -183,13 +197,14 @@ class Strategy(BaseStrategy):
             'lookback_window': 180,
             'train_window': 18000,
             'pos_size': 1000,
+            "take_profit_pct_threshold": 0.001
         },
         
         # Backtest configuration
         "backtest_config": {
             "pair_list": ["BTC"],
-            "start_date": datetime(2025, 2, 1),
-            "end_date": datetime(2025, 2, 5),
+            "start_date": datetime(2024, 1, 1),
+            "end_date": datetime(2024, 1, 2),
             'resample_freq_in_ms': 1000,
             'use_ob_levels': 1,
             'make_ml_predictions': True
@@ -199,6 +214,8 @@ class Strategy(BaseStrategy):
     def __init__(self, pair_list, params):
         super().__init__(pair_list, params)
         self.model_object = Model()
+        self.is_profit_taken = True
+        self.last_take_profit_pos = 0
 
     def make_step_controls(self, pair):
         """Make step controls for the strategy"""
@@ -210,7 +227,7 @@ class Strategy(BaseStrategy):
 
         prediction = self.get_prediction(self.get_current_step(), 'example_ml_model')
         if prediction is None:
-            #self.printer_method(f'Prediction is None. Skipping step: {self.get_current_step()}')
+            # self.printer_method(f'Prediction is None. Skipping step: {self.get_current_step()}')
             return
         
         target_position_value = self.get_target_position_value(prediction)
@@ -218,15 +235,7 @@ class Strategy(BaseStrategy):
         self.send_new_orders(pair, target_position_value)
 
         if self.get_current_step() % 100000 == 0:
-            historical_data = self.get_historical_data(
-                step = self.get_current_step(),
-                lookback_in_seconds=self.params['train_window'],
-                pair = self.pair_list[0]
-            )
-            performance_metrics = self.model_object.get_performance_metrics(historical_data)
-            current_ts_in_ms = self.get_data('timestamp', self.get_current_step())
-            current_datetime = datetime.fromtimestamp(current_ts_in_ms / 1000)
-            self.printer_method(f'{current_datetime} last 24 hour model performance metrics:\n {performance_metrics}')
+            self.send_accuracy_metrics()
 
     def send_new_orders(self, pair, target_position_value):
         """Execute trades for the strategy"""
@@ -243,7 +252,8 @@ class Strategy(BaseStrategy):
                 "side": "buy", 
                 "price": price, 
                 "amount": abs(target_pos_diff),
-                "step": step
+                "step": step,
+                "maker": True
             }
             self.create_limit_order(_dict)
         elif target_pos_diff < 0:
@@ -253,16 +263,45 @@ class Strategy(BaseStrategy):
                 "side": "sell", 
                 "price": price, 
                 "amount": abs(target_pos_diff),
-                "step": step
+                "step": step,
+                "maker": True
             }
             self.create_limit_order(_dict)
     
     def get_target_position_value(self, prediction):
         """Get the target position value for the strategy"""
-        if prediction == 1:
-            return self.params['pos_size']
+        current_pos = self.get_position(self.pair_list[0])
+        current_profit_pct = self.get_pos_profit_pct(self.pair_list[0])
+        
+        if current_pos == 0:
+            self.is_profit_taken = True
+
+        if self.is_profit_taken:
+            if self.last_take_profit_pos < 0: 
+                if prediction == 0:
+                    # Already taken profit, no need to open new position
+                    return 0
+                else:
+                    # side change, open new position
+                    self.is_profit_taken = False
+                    self.last_take_profit_pos = 0
+                    return self.params['pos_size']
+            else:
+                if prediction == 0:
+                    # side change, open new position
+                    self.is_profit_taken = False
+                    self.last_take_profit_pos = 0
+                    return self.params['pos_size']
+                else:
+                    # Already taken profit, no need to open new position
+                    return 0
         else:
-            return -self.params['pos_size']
+            if current_profit_pct > self.params['take_profit_pct_threshold']:
+                self.is_profit_taken = True
+                self.last_take_profit_pos = current_pos
+                return 0
+            else:
+                return current_pos
 
     def train_model(self):
         """Train the model"""
@@ -284,7 +323,33 @@ class Strategy(BaseStrategy):
         """Check if the strategy needs to train"""
         return self.model_object.model is None
 
+    def send_accuracy_metrics(self):
+        """Send accuracy metrics to the server"""
+        historical_data = self.get_historical_data(
+            step = self.get_current_step(),
+            lookback_in_seconds=self.params['train_window'],
+            pair = self.pair_list[0]
+        )
+        performance_metrics = self.model_object.get_performance_metrics(historical_data)
+        current_ts_in_ms = self.get_data('timestamp', self.get_current_step())
+        current_datetime = datetime.fromtimestamp(current_ts_in_ms / 1000)
+        self.printer_method(f'{current_datetime} last 24 hour model performance metrics:\n {performance_metrics}')
 
+    def get_pos_profit_pct(self, pair):
+        """Get the profit of the position"""
+        current_position = self.get_position(pair)
+        current_price = self.get_mid_price(self.get_current_step())
+        pos_entry_price = self.get_open_position_price(pair)
+
+        if current_position == 0:
+            return 0
+
+        pos_entry_val = current_position * pos_entry_price
+        current_pos_val = current_position * current_price
+        profit = current_pos_val - pos_entry_val
+        return profit / abs(pos_entry_val)
+
+    
 if __name__ == "__main__":
     from runing_tools import run_strategy_on_server
 
